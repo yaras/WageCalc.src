8ea1b91250f410186a0d8b5b9e6d57a54ee772bc
yaras <yaras6@gmail.com>
2017-12-04 19:41:03 +0100

    Initial UI

--- src/calculator.js
+++ src/calculator.js
@@ -50,46 +50,50 @@ export default class Calculator extends Component {
     }
 
     render() {
-        return <div className="container" style={{ 'textAlign': 'center' }}>
-            <form onSubmit={e => { e.preventDefault(); this.calc() } }>
-            
-                <div className="input-group mb-2 mr-sm-2 mb-sm-0">                
-                    <div className="input-group-addon">Pensja brutto</div>
-                    <input
-                        type="number"
-                        className={this.state.isValueValid ? "form-control" : "form-control is-invalid " }
-                        onChange={e => this.setBrutto(e)} />
-                    <div className="input-group-addon">zł</div>
-                </div>
+        return <div>
+            <h1>Kalkulator pensji</h1>
 
-                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                    <div className="input-group-addon">Koszty miejscowe</div>
+            <div className="container" style={{ 'textAlign': 'center' }}>
+                <form onSubmit={e => { e.preventDefault(); this.calc() } }>
+                
+                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">                
+                        <div className="input-group-addon">Pensja brutto</div>
+                        <input
+                            type="number"
+                            className={this.state.isValueValid ? "form-control" : "form-control is-invalid " }
+                            onChange={e => this.setBrutto(e)} />
+                        <div className="input-group-addon">zł</div>
+                    </div>
 
-                    <select className="form-control" onChange={e => this.setKoszty(e)}>
-                        <option value="111.25">111.25</option>
-                        <option value="139.06">139.06</option>
-                    </select>
+                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                        <div className="input-group-addon">Koszty miejscowe</div>
 
-                    <div className="input-group-addon">zł</div>
-                </div>
+                        <select className="form-control" onChange={e => this.setKoszty(e)}>
+                            <option value="111.25">111.25</option>
+                            <option value="139.06">139.06</option>
+                        </select>
 
-                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                    <div className="input-group-addon">Stawka wypadkowe</div>
-                    <input
-                        type="number"
-                        className={this.state.isValueValid ? "form-control" : "form-control is-invalid " }
-                        defaultValue="6.7"
-                        onChange={e => this.setWypadkowe(e)} />
-                    <div className="input-group-addon">%</div>
-                </div>
-            </form>
+                        <div className="input-group-addon">zł</div>
+                    </div>
 
-            <div className="row align-items-center" style={{ marginTop: '10px' }}>
+                    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+                        <div className="input-group-addon">Stawka wypadkowe</div>
+                        <input
+                            type="number"
+                            className={this.state.isValueValid ? "form-control" : "form-control is-invalid " }
+                            defaultValue="6.7"
+                            onChange={e => this.setWypadkowe(e)} />
+                        <div className="input-group-addon">%</div>
+                    </div>
+                </form>
+
+                <div className="row align-items-center" style={{ marginTop: '10px' }}>
                 <Button disabled={!this.state.isBruttoValid || !this.state.isStawkaWypadkoweValid} 
                     onClick={e => this.calc()} className="btn btn-success btn-lg" >Wylicz</Button>
-            </div>
+                </div>
 
-            { this.state.isCalculated && <Summary brutto={this.state.brutto} kosztyMiejscowe={this.state.kosztyMiejscowe} stawkaWypadkowe={this.state.stawkaWypadkowe} /> }
+                { this.state.isCalculated && <Summary brutto={this.state.brutto} kosztyMiejscowe={this.state.kosztyMiejscowe} stawkaWypadkowe={this.state.stawkaWypadkowe} /> }
+            </div>
         </div>;
     }
 }
---
+++ src/illness.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Illness extends Component {
+    render() {
+        return <div>
+            <h1>Kalkulator chorobowego</h1>    
+        </div>;
+    }
+}
