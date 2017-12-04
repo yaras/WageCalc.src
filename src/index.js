cf1313590b6ab045f73620e78df29db2e195b86b
yaras <yaras6@gmail.com>
2017-12-04 21:14:48 +0100

    Fix paths

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<BrowserRouter basename="/WageCalc">
         <App />
     </BrowserRouter>, 
     document.getElementById('root'));
